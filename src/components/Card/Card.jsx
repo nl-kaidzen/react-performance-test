ae47b5b2f2d2ce56ab398aaf39a3a190dc252461
Andei Khabarov <akhabarow@gmail.com>
2020-03-24 04:43:04 +0300

    Add handleClick based on data-attributes

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,33 +1,27 @@
-import React, { useCallback } from 'react';
-import { useHistory } from 'react-router-dom';
+import React from 'react';
 import styles from './Card.module.scss';
 import IconButton from '../common/IconButton/IconButton';
 import PropTypes from 'prop-types';
 
 const Card = (props) => {
-  const history = useHistory('/');
-  const handleEditCardClick = useCallback(
-    () => history.push(`/info/${props.cardInfo.id}`),
-    [],
-  );
-
-  const handleFavoriteCardClick = useCallback(
+  /*const handleFavoriteCardClick = useCallback(
     () => {
     props.toggleCardFavoriteStatus(props.cardInfo.id);
-  }, []);
+  }, []);*/
   return (
     <li className={styles.card}>
       <h2 className={styles.cardTitle}>{props.cardInfo.title}</h2>
       <p className={styles.cardText}>{props.cardInfo.text}</p>
       <div className={styles.cardButtonWrapper}>
         <IconButton
-          onClick={handleEditCardClick}
+          //onClick={handleEditCardClick}
           type="edit"
           dataId={props.cardInfo.id}
         />
         <IconButton
-          onClick={handleFavoriteCardClick}
+          //onClick={handleFavoriteCardClick}
           type={props.cardInfo.isFavorite ? 'dislike' : 'like'}
+          dataId={props.cardInfo.id}
         />
       </div>
     </li>
--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useHistory } from 'react-router-dom'
 import Card from '../Card/Card';
 import AddCardButton from '../AddCardButton/AddCardButton';
 import HeaderTitle from '../common/HeaderTitle/HeaderTitle';
@@ -6,6 +7,7 @@ import styles from './CardList.module.scss';
 import PropTypes from 'prop-types';
 
 const CardList = (props) => {
+  const history = useHistory('/');
   const cards = (Object.entries(props.cards).map(([key, card]) => (
     <Card 
       cardInfo={card}
@@ -13,16 +15,16 @@ const CardList = (props) => {
       key={key}
     />
   )));
-  const handleClick = (event) => {
+  const handleClick = useCallback((event) => {
     const target = event.target.closest('button');
     if (target) {
       switch (target.dataset.action) {
-        case 'edit': return console.log('Catch edit');
-        case 'like': return console.log('Catch like');
-        case 'dislike': return console.log('Catch dislike')
+        case 'edit': return history.push(`/info/${target.dataset.id}`);
+        case 'like': return props.toggleCardFavoriteStatus(target.dataset.id);
+        case 'dislike': return props.toggleCardFavoriteStatus(target.dataset.id);
       }
     }
-  }
+  }, []);
   return (
     <>
       <HeaderTitle title="Card List" />
